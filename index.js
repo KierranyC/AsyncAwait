af1c2e7de0f5e99a9cd89881ad28f0c8bb04e1aa
KierranyC <kierranyc@outlook.com>
2023-06-22 14:07:26 -0500

    commit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mostFrequentWord = (text) => {
   const words = text.toLowerCase().match(/[^_\W]+/g);
   const tally = {};
   let mostFrequentWord = null;
-  
+
   words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+    tally[word] = (tally[word] || 0) + 1;
+    if (!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
       mostFrequentWord = word;
   });
   return mostFrequentWord;
@@ -15,6 +15,17 @@ const mostFrequentWord = (text) => {
 
 const findPassword = async () => {
   // Your code goes here
+  try {
+    const mostFrequentWord1 = await readFile('poems/starting-poem.txt', 'utf-8')
+    console.log(mostFrequentWord(mostFrequentWord1))
+  }
+  catch (error) {
+    console.error(error)
+  }
+  const mostFrequentWord2 = await readFile('poems/happy.txt', 'utf-8')
+  console.log(mostFrequentWord(mostFrequentWord2))
+  const mostFrequentWord3 = await readFile('poems/you.txt', 'utf-8')
+  console.log(mostFrequentWord(mostFrequentWord3))
 }
 
-findPassword();
+findPassword()
