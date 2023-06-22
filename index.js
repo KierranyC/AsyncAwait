814d73fe33290038ae4aab1f59d02168a9685253
KierranyC <kierranyc@outlook.com>
2023-06-22 16:20:55 -0500

    commit

--- index.js
+++ index.js
@@ -15,13 +15,8 @@ const mostFrequentWord = (text) => {
 
 const findPassword = async () => {
   // Your code goes here
-  try {
-    const mostFrequentWord1 = await readFile('poems/starting-poem.txt', 'utf-8')
-    console.log(mostFrequentWord(mostFrequentWord1))
-  }
-  catch (error) {
-    console.error(error)
-  }
+  const mostFrequentWord1 = await readFile('poems/starting-poem.txt', 'utf-8')
+  console.log(mostFrequentWord(mostFrequentWord1))
   const mostFrequentWord2 = await readFile('poems/happy.txt', 'utf-8')
   console.log(mostFrequentWord(mostFrequentWord2))
   const mostFrequentWord3 = await readFile('poems/you.txt', 'utf-8')
